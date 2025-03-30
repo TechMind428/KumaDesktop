dc49f8aaf9dbf81f835c63845e5cb9f1d41788dd
TechMind Co., Ltd. <shibu@techmind.co.jp>
2025-03-30 12:16:54 +0900

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 ############################################
 # 個別情報の設定 #
 ############################################
-DEVICE_ID = "Aid-80070001-0000-2000-9002-000000000aa3"
-CLIENT_ID = "0oahe4a2pwRF7spHv697"
-CLIENT_SECRET = "uSSDtRqL3HTvqdv6cHVBGZMRoHPG9nOLxldMcubW37YpBU0cCKXs0g9_ozIpg4mH"
+DEVICE_ID = "Aid-80070001-0000-2000-9002-000000000xxx"
+CLIENT_ID = "２０文字の文字列"
+CLIENT_SECRET = "64文字の文字列"
 numberofclass = 89
 numberofclass = 89
 objclass = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', '-', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', '-', 'backpack', 'umbrella', '-', '-', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', '-', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', '-', 'dining table', '-', '-', 'toilet', '-', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', '-', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier']
